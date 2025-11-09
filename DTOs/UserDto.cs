e9b3635b921ca3228d38738e4f1f14796c77a05f
Siraj Khan .H <sirajh2002@gmail.com>
2025-11-09 16:03:47 +0530

    Add files via upload

---
+++ DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PIzzaDeliveryAPI.DTOs
+{
+    public class UserDto
+    {
+        public  int Id { get; set; }//only returning after saving
+        public  string Name { get; set; }
+        public  string Email { get; set; }
+    }
+}
