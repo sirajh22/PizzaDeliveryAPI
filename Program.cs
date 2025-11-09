1fe7d055e34e62f57e9a76ea08cc938260bf325f
Siraj Khan .H <sirajh2002@gmail.com>
2025-11-09 16:08:18 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 
-// Add services to the container.
+
 
 builder.Services.AddControllers()
  .AddJsonOptions(options =>
@@ -45,13 +45,13 @@ builder.Services.AddAuthentication(options =>
     });
 builder.Services.AddAuthorization();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -67,3 +67,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
